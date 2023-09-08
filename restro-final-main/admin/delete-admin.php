0b4bd647fa1dcacdaf1653987f5c8a8412fa199c
geshal26 <84404533+geshal26@users.noreply.github.com>
2023-09-08 12:37:18 +0530

    Update delete-admin.php

--- restro-final-main/admin/delete-admin.php
+++ restro-final-main/admin/delete-admin.php
@@ -1,6 +1,5 @@
 <?php
-//1.get id of admin to delete
-   //include constants .php here
+//get id of admin to delete
 
    include('../config/constants.php');
    echo $id = $_GET['id'];
@@ -15,7 +14,6 @@
   if($res==TRUE)
   {
     //query exceuted and admin deleted
-    //echo "ADMIN DELETED";
 
    // cretae session variable 
       $_SESSION['delete'] = "<div class='success'>Admin Deleted Succesfully</div>";
@@ -29,26 +27,6 @@
     $_SESSION['delete'] = "<div class='error'>Failed to Delete Admin</div>";
     header("location:".SITEURL.'admin/manage-admin.php');
 
-
-    //echo " FAILED TO DELETE ADMIN";
   }
-
-//2.create  sql qury to dlete admin
-
-
-
-
-
-
-
-
-//3.redirect to manage admin page with msg 
-
-
-
-
-
-
-
 
 ?>
